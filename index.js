373cb2490f3aa651215e4f4251276b3ddccce00f
Atanu Nayak <145501388+nayak-s-horcrux@users.noreply.github.com>
2024-02-24 16:43:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const app = express();
 
-app.get('/problem0/:number', (req, res) => {
-    const number = parseInt(req.params.number);
-    const square = number * number;
-    res.send(`The square of ${number} is ${square}`);
-});
+class Tokeniser {
+    constructor(str) {
+        this.tokens = str.split(/\W+/);
+        this.nextToRead = 0;
+    }
+    read() {
+        if (this.nextToRead < this.tokens.length) {
+            return this.tokens[this.nextToRead++];
+        } else {
+            throw "error";
+        }
+    }
+}
 
-app.post('/problem1', express.json(), (req, res) => {
-    const { n, array } = req.body;
-    const sum = array.reduce((acc, curr) => acc + curr, 0);
-    res.send(`The sum of the array ${JSON.stringify(array)} is ${sum}`);
+function problem_0(input) {
+    var inp = new Tokeniser(input);
+    var out = "";
+    var tt = parseInt(inp.read());
+    while (tt--) {
+        var n = parseInt(inp.read());
+        var deg = Array(n + 5).fill(0);
+        for (var i = 0; i < n - 1; i++) {
+            var u = parseInt(inp.read());
+            var v = parseInt(inp.read());
+            deg[u]++;
+            deg[v]++;
+        }
+        var ans = 0;
+        for (var i = 1; i <= n; i++) {
+            if (deg[i] == 1) {
+                ans++;
+            }
+        }
+        out += ans + "\n";
+    }
+    return out;
+}
+
+app.post('/problem3', express.text(), (req, res) => {
+    const input = req.body;
+    const result = problem_0(input);
+    res.send(result);
 });
 
 app.listen(process.env.PORT || 3000, () => {
