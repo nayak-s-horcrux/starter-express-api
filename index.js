bb434f88b9f269613df0b6620dcc3145bee8a7db
Atanu Nayak <145501388+nayak-s-horcrux@users.noreply.github.com>
2024-02-24 13:04:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+app.get('/problem0/:number', (req, res) => {
+    const number = parseInt(req.params.number);
+    const square = number * number;
+    res.send(`The square of ${number} is ${square}`);
+});
+
+app.post('/problem1', express.json(), (req, res) => {
+    const { n, array } = req.body;
+    const sum = array.reduce((acc, curr) => acc + curr, 0);
+    res.send(`The sum of the array ${JSON.stringify(array)} is ${sum}`);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running...');
+});
